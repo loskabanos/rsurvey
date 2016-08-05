95872b3a25aed1720bb93036dfe1b1339154e0a0
loskabanos <loskaban@gmail.com>
2016-08-06 01:17:39 +0300

    добавил комментарии

--- sql/добавление эвентов.sql
+++ sql/добавление эвентов.sql
@@ -1,11 +1,13 @@
 select * from `tevent`
 
+--- русский язык, основная запись
 insert into `tevent` (devent, sname, blogo, bhotel, osiformula, nBGColorR, nBGColorG, nBGColorB,
 nFontColorR, nFontColorG, nFontColorB, sWelcomeText1, sWelcomeText2, sWelcomeText3, nidorg, nidlang)
 select '2015-11-01', sname, blogo, bhotel, osiformula, nBGColorR, nBGColorG, nBGColorB,
 nFontColorR, nFontColorG, nFontColorB, sWelcomeText1, sWelcomeText2, sWelcomeText3, nidorg, nidlang
 from tevent where nidevent = 28;
 
+--- английский язык
 insert into `tevent` (sname, sWelcomeText1, sWelcomeText2, sWelcomeText3, nidorg, nidlang, nideventparent)
 select sname, sWelcomeText1, sWelcomeText2, sWelcomeText3, nidorg, nidlang, 71
 from tevent where nideventparent = 28;
